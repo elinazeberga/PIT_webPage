3e78fc1e803c841fd5861804164a69ceec1de6ac
Colorato291 <regnars.slapins@gmail.com>
2024-11-28 15:38:54 +0200

    Feat: deleted entry handling

--- src/renderers/reservationRenderer.js
+++ src/renderers/reservationRenderer.js
@@ -1,19 +1,24 @@
 const {formatDate} = require('../utils/dateUtils');
 
-function createReservationTableRow(booking, usersMap, vehiclesMap) {
+function createReservationTableRow(booking, usersMap, vehiclesMap) { // Defines how a reservation table row should be displayed
+    // Get associated data
     const vehicle = vehiclesMap.get(booking.car.toString());
     const user = usersMap.get(booking.user.toString());
 
-    if (!vehicle || !user) {
-        console.error('Missing vehicle or user data for reservation:', booking._id);
-        return '';
-    }
+    // Handle cases when vehicle or user is deleted
+    const vehicleData = vehicle ?
+    `<td>${vehicle.registrationNumber}</td>
+    ­<td>${vehicle.make} ${vehicle.model}</td>`: 
+    `<td>DELETED</td> <td>${booking.car}</td>`;
 
+    // Handle cases when vehicle or user is deleted
+    const userData = user ?
+    `<td>${user.name} ${user.lastName}</td>`: 
+    `<td>DELETED ${booking.user}`;
     return `
         <tr>
-            <td>${vehicle.registrationNumber}</td>
-            <td>${vehicle.make} ${vehicle.model}</td>
-            <td>${user.name} ${user.lastName}</td>
+            ${vehicleData}
+            ${userData}
             <td>${formatDate(booking.rentalStartDate)}</td>
             <td>${formatDate(booking.rentalEndDate)}</td>
             <td>${booking.status}</td>
